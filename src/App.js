6d253ab8f1e4162754976c9df7ec9092e846ccbf
alejocastagit <alejo.casta.al@gmail.com>
2020-05-02 13:36:02 -0500

    56. Outputting Lists

--- src/App.js
+++ src/App.js
@@ -51,9 +51,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons=(
         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} change={this.changeNameHandler} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.changePersonsHandler.bind(this, 'Ramas', 24)}>Boyfriend: Alejandro </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age}/> 
+          })}
         </div>
       )
     }
