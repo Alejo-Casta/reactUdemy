5d06d10ee8e1ea0773d720e0047a8dad30ddc338
alejocastagit <alejo.casta.al@gmail.com>
2020-05-02 13:18:04 -0500

    53. Rendering Content Conditionally

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       { name: 'Alejandro', age: 24 },
       { name: 'Lina', age: 23 },
       { name: 'Teo', age: 24 }
-    ]
+    ],
+    showPersons: false
   }
 
   changePersonsHandler = (friend, age) => {
@@ -31,6 +32,11 @@ class App extends Component {
     })
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({showPersons: !doesShow})
+  }
+
   render(){
     const style = {
       backgroundColor: 'white',
@@ -43,10 +49,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Primer pagina</h1>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} change={this.changeNameHandler}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.changePersonsHandler.bind(this, 'Ramas', 24)}>Boyfriend: Alejandro </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        <button style={style} onClick={() => this.changePersonsHandler('Milo', 25)}>Change Persons</button> {/*Menos eficiente*/}
+        <button style={style} onClick={() => this.togglePersonsHandler('Milo', 25)}>Show Persons</button> {/*Menos eficiente*/}
+        {this.state.showPersons === true ?
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} change={this.changeNameHandler}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.changePersonsHandler.bind(this, 'Ramas', 24)}>Boyfriend: Alejandro </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        </div> : null
+        }
       </div>
     );
   }
