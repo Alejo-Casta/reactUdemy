279408db91b151f95b5f907db5c2ab601ec27177
alejocastagit <alejo.casta.al@gmail.com>
2020-05-05 22:25:49 -0500

    91. Component Update Lifecycle (for props Changes)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import style from './Person.module.css';
 
-const Person = (props) => {
-    console.log('[Person.js] rendering...')
-    return (
-        <div className={style.Person}>
-            <p>Hello i am {props.name}, I am {props.age} years old</p>
-            <input className={style.input} type="text" onChange={props.change} value={props.name} /><br />
-            <button className={style.button} onClick={props.click}>Delete</button>
-        </div>
-    )
+class Person extends React.Component {
+    render() {
+        console.log('[Person.js] rendering...')
+        return (
+            <div className={style.Person} >
+                <p>Hello i am {this.props.name}, I am {this.props.age} years old</p>
+                <input className={style.input} type="text" onChange={this.props.change} value={this.props.name} /><br />
+                <button className={style.button} onClick={this.props.click}>Delete</button>
+            </div>
+        )
+    }
 }
 
 export default Person;
--- src/components/Persons/persons.js
+++ src/components/Persons/persons.js
@@ -2,15 +2,34 @@ import React from 'react';
 
 import Person from './Person/Person'
 
-const Persons = (props) => props.persons.map((person, i) => {
-    console.log('[Persons.js] rendering...')
-    return <Person
-        key={person.id}
-        name={person.name}
-        age={person.age}
-        click={() => props.delete(i)}
-        change={(e) => props.change(e, person.id)}
-    />
-})
+class Persons extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate')
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate')
+        return { message: 'Snapshot!' }
+    }
+
+    componentDidUpdate(prevProps, prevState, Snapshot) {
+        console.log('[Persons.js] componentDidUpdate')
+        console.log(Snapshot)
+    }
+
+    render() {
+        console.log('[Persons.js] rendering...')
+        return this.props.persons.map((person, i) => {
+            return <Person
+                key={person.id}
+                name={person.name}
+                age={person.age}
+                click={() => this.props.delete(i)}
+                change={(e) => this.props.change(e, person.id)}
+            />
+        })
+    }
+}
 
 export default Persons; 
